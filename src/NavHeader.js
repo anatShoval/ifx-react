a35a8e6b0e13d1fff9eb2924388a3cfe3b5f981c
Anat Knebel Shoval <anat.shoval@gmail.com>
2017-12-04 13:07:04 +0200

    semanticUi

--- src/NavHeader.js
+++ src/NavHeader.js
@@ -5,13 +5,17 @@ import './NavHeader/NavHeader.css';
 
 const NavHeader = (props) => {
     return (
-      <div className="container">
+      <div className="container row">
+        <div className="four wide column">
         <a><img className="logo" src={logo} alt={logo}/></a>
+        </div>
+        <div className = "four wide column">
         <SearchBar
           searchTerm = {props.searchTerm}
           searchUpdated={(value) => props.searchUpdated.bind(value)}
           movies={props.movies}
         />
+        </div>
       </div>
     );
 }
